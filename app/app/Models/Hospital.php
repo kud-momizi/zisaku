91d20e1e5a31e1128d05270df1a5b93f18b6489f
koyo <koyo.yazaki@gmail.com>
2023-05-06 12:20:08 +0900

    HospitalモデルからAuthenticatableトレイトを削除

--- app/app/Models/Hospital.php
+++ app/app/Models/Hospital.php
@@ -6,23 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hospital extends Model
 {
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    //
 }
 
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * Get the hospital record associated with the user.
-     */
-    public function hospital()
-    {
-        return $this->hasOne(Hospital::class);
-    }
-
-    // 他のコード
-}
